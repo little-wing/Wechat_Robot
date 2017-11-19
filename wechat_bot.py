0171ae6daed33ac68141b3abc924e9adf495d2c4
little-wing <little_wing@hotmail.com>
2017-11-19 22:25:56 +0800

    Add files via upload

--- wechat_bot.py
+++ wechat_bot.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 import sys
-from wxbot import WXBot
+import time
+from wxbot import *
 
 class myWechatBot(WXBot):
 	def handle_msg_all(self, msg):
-		if msg['msg_type_id'] == 5:
-			self.send_msg(msg['user_name'], 'hi')
+		print msg
+		for (k, v) in msg.items():
+			print "%s => %s" % (k, v)
+		#群聊 msg_type_id 为 3
+		if msg['msg_type_id'] == 4:
+			print "==================="
+			print msg['user']['name']
+#			self.send_msg(msg['user']['name'], msg['content']['data'])
+			self.send_msg_by_uid("Message sent by uid!", msg['user']['id'])
+			print "==================="
+
+#		else:
+#			self.send_msg('benyu_cn', 'good')
+			
+	def schedule(self):
+		self.send_msg('benyu_cn', 'this is schedule')
+		time.sleep(2)
+
+def main():
+	bot = myWechatBot()
+	bot.DEBUG = True
+	bot.run()
 		
 if __name__ == '__main__':
-	bot = myWechatBot()
-	bot.run()
+	main()
