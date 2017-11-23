5c22350a9fb7265b1ce368d7c1de6ce8d1543113
little-wing <little_wing@hotmail.com>
2017-11-23 23:45:42 +0800

    Add files via upload

--- wechat_bot.py
+++ wechat_bot.py
@@ -7,16 +7,17 @@ from wxbot import *
 
 class myWechatBot(WXBot):
 
-	wechat_DB = 'D:\SQLite\DB_Files\wechatBotDB.db'
+#	wechat_DB = 'D:\SQLite\DB_Files\wechatBotDB.db'
+	wechat_DB = './wechatBotDB.db'
 	def handle_msg_all(self, msg):
-#		print msg
 		print "=========================================================================="
+		print msg
 		for (k, v) in msg.items():
 			print "%s => %s" % (k, v)
 		# group msg_type_id = 3
-#		if msg['msg_type_id'] == 3:
+		if msg['msg_type_id'] == 3:
 		# contact msg_type_id = 4
-		if msg['msg_type_id'] == 4:
+#		if msg['msg_type_id'] == 4:
 			print "******************"
 			print msg['user']['id']
 			print msg['user']['name']
@@ -33,32 +34,41 @@ class myWechatBot(WXBot):
 			
 			# Insert data into DB
 			# time_stamp is the primary key in chat_history
-			if msg['user']['id'] == '@fb7d161360c0c80b0aa8d0f5e2dd2a07':
-				time_stamp = time.time()
-				current_date = time.strftime("%Y-%m-%d", time.localtime())	
-				current_time = time.strftime("%H:%M:%S", time.localtime())
+			#if msg['user']['id'] == '@fb7d161360c0c80b0aa8d0f5e2dd2a07':
+			time_stamp = time.time()
+			current_date = time.strftime("%Y-%m-%d", time.localtime())	
+			current_time = time.strftime("%H:%M:%S", time.localtime())
 #				insert_chatHistory = (time_stamp, msg['user']['id'], current_date, current_time, msg['content']['data'])
 #				cur.execute("INSERT INTO chat_history VALUES (?, ?, ?, ?, ?)", insert_chatHistory)
-				cur.execute("INSERT INTO chat_history VALUES (?, ?, ?, ?, ?)", (time_stamp, msg['user']['id'], current_date, current_time, msg['content']['data']))
-				conn.commit()		
+			cur.execute("INSERT INTO chat_history VALUES (?, ?, ?, ?, ?)", (time_stamp, msg['user']['id'], current_date, current_time, msg['content']['data']))
+			conn.commit()	
+			conn.close()			
 				
 				
-			if msg['content']['data'] == 'today':
-				current_date = time.strftime("%Y-%m-%d", time.localtime())
-				teacher_instruction = cur.execute("SELECT CONTENT FROM chat_history WHERE DATE=?", (current_date,))
-				for line in teacher_instruction:
-					print line
-
+		if msg['msg_type_id'] == 4 and msg['content']['data'] == 'today':
+			current_date = time.strftime("%Y-%m-%d", time.localtime())
+			conn = sqlite3.connect(self.wechat_DB)
+			cur = conn.cursor()
+			teacher_instruction = cur.execute("SELECT TIME, CONTENT FROM chat_history WHERE DATE=?", (current_date,))
+			output_str = ""
+			for line in teacher_instruction:
+				output_str = output_str + line[0] + '\n' + line[1] + '\n\n'	
+			self.send_msg_by_uid(output_str, msg['user']['id'])
 			conn.close()
+			
 #			print msg['user']['name']
 #			self.send_msg(msg['user']['name'], msg['content']['data'])
 #			self.send_msg_by_uid("群聊消息收到", msg['user']['id'])
 
+##########################################################################################################
+		# Code block for forwarding pic(gaowc)
+		if msg['content']['type'] == 3:
+			received_file = self.get_msg_img(msg['msg_id'])
 			
 
 def main():
 	bot = myWechatBot()
-	bot.DEBUG = True
+#	bot.DEBUG = True
 	bot.run()
 		
 if __name__ == '__main__':
